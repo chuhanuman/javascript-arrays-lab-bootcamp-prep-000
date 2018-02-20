f54cf68110deecb71b459a86605d8a0ae28188dc
Hanuman Chu <chuhanuman@gmail.com>
2018-02-20 18:59:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+function prependKitten(name) {
+  
+}
+function appendKitten(name) {
+  
+}
+function removeFirstKitten() {
+  
+}
+function removeLastKitten() {
+  
+}
